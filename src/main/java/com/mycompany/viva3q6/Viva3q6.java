0df3278e008383c925ba3cf19411695439318ee2
nazhanfahim <23002679@siswa.um.edu.my>
2024-12-26 11:22:44 +0800

    minor updates

---
+++ src/main/java/com/mycompany/viva3q6/Viva3q6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.viva3q6;
+
+/**
+ *
+ * @author Dell
+ */
+public class Viva3q6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
